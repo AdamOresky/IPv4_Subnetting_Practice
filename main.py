09a05a91894546fac4b51848c2af35718ece7f6c
oresky.adam@gmail.com <oresky.adam@gmail.com>
2022-06-26 21:38:11 +0200

    Error messages, code cleanup

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 
 # region Global variables
 
-classes = ['A', 'B', 'C', 'D', 'E']
 octets = []
 mask = 0
 
@@ -31,17 +30,20 @@ def check_possible_errors():
         sys.exit()
 
 
+# A function that prints message
+def incorrect_answer(message):
+    print(message)
+
+
 # A function that returns a class of an IP according to its first octet
-def class_logic():
+def get_class():
+    classes = ['A', 'B', 'C', 'D', 'E']
     interfaces = [128, 192, 224, 240, 256]
-
-    for i in range(len(interfaces)):
-        if octets[0] < interfaces[i]:
-            return classes[i]
+    return classes[interfaces.index(min(interfaces, key=lambda x: x < octets[0]))]
 
 
-# A function that returns whether the IP address is private or public
-def private_logic():
+# A function that returns whether the IP address is private
+def is_private():
     option_1 = octets[0] == 10  # 10.X.X.X (class A)
     option_2 = octets[0] == 100  # 100.X.X.X (Carrier NAT)
     option_3 = octets[0] == 165 and octets[1] == 254  # 165.254.X.X (APIPA)
@@ -50,43 +52,41 @@ def private_logic():
     option_6 = octets[0] == 192 and octets[1] == 168  # 192.168.X.X (class C)
 
     if sum([option_1, option_2, option_3, option_4, option_5, option_6]):
-        return "pri"
-    else:
-        return "pub"
+        return True
+    return False
 
 
-# A function that returns the type (Network / Host / Broadcast) of an IP address and its attributes
-def type_logic():
-    type_of_address, net_address, bro_address = '', '', ''
+# A function that returns the type (Network / Host / Broadcast) of an IP address and its network and broadcast address
+def get_info():
+    net_address = ''
     actual = 0
     interfaces = [8, 16, 24, 32]
 
-    for a in range(len(interfaces)):
-        if mask < interfaces[a]:
-            multiples = 2 ** (interfaces[a] - mask)
-            while actual < 256:
-                if actual <= octets[a] <= actual + multiples - 1:
-                    for b in range(a):
-                        net_address += str(octets[b]) + '.'
-                    bro_address = net_address
+    interface = min(interfaces, key=lambda x: x <= mask)
+    multiples = 2 ** (interface - mask)
+    while actual < 256:
+        if actual <= octets[interfaces.index(interface)] <= actual + multiples - 1:
+            for b in range(interfaces.index(interface)):
+                net_address += str(octets[b]) + '.'
+            bro_address = net_address
 
-                    net_address += str(actual)
-                    bro_address += str(actual + multiples - 1)
+            net_address += str(actual)
+            bro_address += str(actual + multiples - 1)
 
-                    while net_address.count('.') < 3:
-                        net_address += ".0"
-                        bro_address += ".255"
+            while net_address.count('.') < 3:
+                net_address += ".0"
+                bro_address += ".255"
 
-                    if octets == net_address.split('.'):
-                        type_of_address = 'N'
-                    elif octets == bro_address.split('.'):
-                        type_of_address = 'B'
-                    else:
-                        type_of_address = 'H'
+            if octets == net_address.split('.'):
+                address_type = 'N'
+            elif octets == bro_address.split('.'):
+                address_type = 'B'
+            else:
+                address_type = 'H'
 
-                    return type_of_address, net_address, bro_address
-                else:
-                    actual += multiples
+            return address_type, net_address, bro_address
+        else:
+            actual += multiples
 
 # endregion
 
@@ -95,55 +95,53 @@ def type_logic():
 # In this region, there are only functions that checks for user input and evaluate it
 
 def class_input():
-    right_answer = class_logic()
+    right_answer = get_class()
 
     entered_answer = input("Enter class: ").upper()
-    while entered_answer not in classes:
+    while entered_answer not in ['A', 'B', 'C', 'D', 'E']:
         entered_answer = input("Incorrect input. Enter class: ").upper()
     if entered_answer != right_answer:
-        input(f'Incorrect. The correct answer is {right_answer}.')
+        incorrect_answer(f'Incorrect. The correct answer is {right_answer}.')
 
 
 def private_input():
-    right_answer = private_logic()
-
     entered_answer = input("Specify whether the address is private or public. [pri/pub]: ").lower()
     while entered_answer not in ['pri', 'pub']:
         entered_answer = input("Incorrect input. Enter your answer again: ").lower()
-    if entered_answer != right_answer and right_answer == "pri":
-        input("Incorrect. This address is private.")
-    elif entered_answer != right_answer and right_answer == "pub":
-        input("Incorrect. This address is public.")
+    if entered_answer == "pub" and is_private():
+        incorrect_answer("Incorrect. This address is private.")
+    elif entered_answer == "pri" and not is_private():
+        incorrect_answer("Incorrect. This address is public.")
 
 
-def subnet_input(type_of_address, net_address, bro_address):
+def subnet_input(address_type, net_address, bro_address):
     address_base = ''
-    for i in range(0, mask // 8):
+    for i in range(mask // 8):
         address_base += str(octets[i]) + '.'
-    if type_of_address != 'N':
+    if address_type != 'N':
         entered_answer = input(f'Enter network address: {address_base}')
         if address_base + entered_answer != net_address:
-            input(f'Incorrect. Network address is {net_address}')
-    if type_of_address != "B":
+            incorrect_answer(f'Incorrect. Network address is {net_address}')
+    if address_type != "B":
         entered_answer = input(f'Enter broadcast address: {address_base}')
         if address_base + entered_answer != bro_address:
-            input(f'Incorrect. Broadcast address is {bro_address}')
+            incorrect_answer(f'Incorrect. Broadcast address is {bro_address}')
 
 
 def type_input():
-    type_of_address, net_address, bro_address = type_logic()
+    address_type, net_address, bro_address = get_info()
 
     entered_answer = input("Specify whether this is a network, host, or broadcast address. [N/H/B]: ").upper()
     while entered_answer not in ['N', 'H', 'B']:
-        entered_answer = input("Incorrect input. Enter your input again: ")
-    if entered_answer != type_of_address and type_of_address == 'N':
-        input("Incorrect. This IP is a network address.")
-    elif entered_answer != type_of_address and type_of_address == 'H':
-        input("Incorrect. This IP is a host address.")
-    elif entered_answer != type_of_address:
-        input("Incorrect. This IP is a broadcast address.")
+        entered_answer = input("Incorrect input. Enter your input again: ").upper()
+    if entered_answer != address_type and address_type == 'N':
+        incorrect_answer("Incorrect. This IP is a network address.")
+    elif entered_answer != address_type and address_type == 'H':
+        incorrect_answer("Incorrect. This IP is a host address.")
+    elif entered_answer != address_type:
+        incorrect_answer("Incorrect. This IP is a broadcast address.")
 
-    subnet_input(type_of_address, net_address, bro_address)
+    subnet_input(address_type, net_address, bro_address)
 
 # endregion
 
